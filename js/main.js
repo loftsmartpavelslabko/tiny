002577d6e0ceeaa850637ac7c92abe9597cefa69
Mikalai Bury <burymm@gmail.com>
2017-06-15 00:58:52 +0300

    update structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#side-mn').addEventListener('click', function() {
-        if (document.body.className.includes('active')) {
-            document.body.className = document.body.className.replace(' active', '');
-        } else {
-            document.body.className += ' active';
-        }
-    })
+    document.querySelectorAll('.side-menu').forEach(function (item) {
+        item.addEventListener('click', function() {
+            if (item.className.includes('active')) {
+                item.className = item.className.replace(' active', '');
+            } else {
+                item.className += ' active';
+            }
+        })
+    });
 
 });
